d33eb1b25397beaea5a55c40542b5a7c086027ec
Dominic52 <dongyuanyang@hotmail.com>
2020-04-16 14:58:29 +1200

    added click events

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-alert("asdf")
+const buttonColour = ["red", "blue", "green", "yellow"];
+
+let gamePattern = [];
+
+let randomChosenColour = buttonColour[nextSequence()];
+
+function nextSequence(){
+    return Math.floor(Math.random()*3);
+}
+
+function btnClick (colour){
+    $(colour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+    let audioStr = "/sounds/" + colour + ".mp3"
+    let a = new Audio(audioStr);
+    a.play();
+
+}
+
+$(".btn").click(function(){
+    btnClick(this.id);
+});
